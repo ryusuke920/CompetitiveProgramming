282507892952c7edd982d217d19708a35a78f376
ryusuke920 <ryusuke_920@icloud.com>
2022-11-04 20:46:39 +0900

    solve problem

--- AtCoder/ABC/ABC266/D/D.py
+++ AtCoder/ABC/ABC266/D/D.py
@@ -17,7 +17,32 @@ import sys
 input = sys.stdin.readline
 
 def main() -> None:
-    pass
+    n = int(input())
+    p = [list(map(int, input().split())) for _ in range(n)]
+    
+    # dp[i][j] := 時刻 i に座標 j にいるときの合計の最大値
+    max_t = p[-1][0]
+    dp = [[0] * 5 for _ in range(max_t + 1)]
+
+    snuke = [[0] * 5 for _ in range(max_t + 1)]
+    for i in range(n):
+        t, x, a = p[i]
+        if t <= 3 and t < x:
+            continue
+        snuke[t][x] = a
+
+    for i in range(1, max_t + 1):
+        for j in range(5):
+            if j == 0:
+                dp[i][j] = max(dp[i][j], dp[i - 1][j + 1], dp[i - 1][j]) + snuke[i][j]
+            elif j == 4:
+                dp[i][j] = max(dp[i][j], dp[i - 1][j - 1], dp[i - 1][j]) + snuke[i][j]
+            else:
+                dp[i][j] = max(dp[i][j], dp[i - 1][j + 1], dp[i - 1][j - 1], dp[i - 1][j]) + snuke[i][j]
+        
+
+    print(max(dp[max_t]))
+
 
 if __name__ == "__main__":
     main()
