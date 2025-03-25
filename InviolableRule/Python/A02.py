f9b821d842a1c31cedfec38190aaf870e73de31d
ryusuke920 <ryusuke_920@icloud.com>
2025-03-25 22:00:59 +0900

    rm InvioableRule/*

--- InviolableRule/Python/A02.py
+++
@@ -1,7 +0,0 @@
-n, x = map(int, input().split())
-a = list(map(int, input().split()))
-
-if x in a:
-    print("Yes")
-else:
-    print("No")
--- InviolableRule/Python/A04.py
+++
@@ -1,3 +0,0 @@
-n = int(input())
-s = bin(n)[2:]
-print("0" * (10 - len(s)) + s)
