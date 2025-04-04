c1723e056734804ef66ee6fd2e3af69e5934447d
ryusuke920 <ryusuke_920@icloud.com>
2025-04-04 22:15:26 +0900

    solve A

--- AtCoder/ABC/ABC394/A/A.py
+++ AtCoder/ABC/ABC394/A/A.py
@@ -1,26 +1,8 @@
-'''
-oj（online-judge-tools）の使い方について
+S = input()
 
-1. テストケースをダウンロード
-2. サンプルが合っているかジャッジする
-3. 提出する
+ans = ""
+for i in S:
+    if i == "2":
+        ans += i
 
-oj d https://atcoder.jp/contests/abc394/tasks/abc394_a
-oj t -c "python3 A.py"
-oj s https://atcoder.jp/contests/abc394/tasks/abc394_a A.py --guess-python-interpreter pypy
-
-※test/ が既に作成されている場合は下記コマンドで test/ を削除する
-rm -rf test/
-'''
-
-def main() -> None:
-    S = input()
-    ans = ""
-    for i in S:
-        if i == "2":
-            ans += i
-    print(ans)
-    
-
-if __name__ == "__main__":
-    main()
+print(ans)
